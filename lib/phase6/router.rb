2fe04a600f5ef83d5156983b149a415ecb8ea70d
Theo <acwazytomato@gmail.com>
2015-09-23 13:22:44 -0700

    finished rails lite part 1

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -18,7 +18,16 @@ module Phase6
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
     def run(req, res)
-      controller = send(@controller_class.new(req,res))
+      route_params = {}
+
+      regex = Regexp.new(@pattern)
+      match_data = regex.match(req.path)
+
+      match_data.names.each do |key|
+        route_params[key] = match_data[key]
+      end
+      p @controller_class
+      controller = @controller_class.new(req,res,route_params)
       controller.invoke_action(@action_name)
     end
   end
@@ -38,12 +47,13 @@ module Phase6
     # evaluate the proc in the context of the instance
     # for syntactic sugar :)
     def draw(&proc)
+      instance_eval(&proc)
     end
 
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
-      define_method("#{http_method}(pattern,controller_class,action_name)") do
+      define_method(http_method) do |pattern, controller_class, action_name|
           add_route(pattern, http_method, controller_class, action_name)
       end
     end
